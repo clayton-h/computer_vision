043cedbb26ce84854ab660220aca714a182dec10
ClayT <claytonhodges326@gmail.com>
2024-09-10 09:52:59 -0600

    hw2_initial_commit

--- lecture/main.py
+++ lecture/main.py
@@ -1,20 +1,17 @@
 import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL.GimpGradientFile import linear
-
 
 def main() -> None:
-    window = "BlurExample"
-    img = cv2.imread('snek.jpg')
-
-    blurred_image = cv2.GaussianBlur(img, (3, 3), 1.5)
-
-    cv2.imshow(window, img)
-    cv2.imshow(window+"2", blurred_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    pass
+    print("main")
+    # window = "BlurExample"
+    # img = cv2.imread('snek.jpg')
+    #
+    # blurred_image = cv2.GaussianBlur(img, (3, 3), 1.5)
+    #
+    # cv2.imshow(window, img)
+    # cv2.imshow(window+"2", blurred_image)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # pass
 
 if __name__ == '__main__':
     main()
